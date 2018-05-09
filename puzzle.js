76e148a64c8a4122328455b8f43fdab808c72645
josy608 <joselyn608@gmail.com>
2018-05-09 11:14:46 +0200

    nuevo ficheros

---
+++ puzzle.js
@@ -0,0 +1,50 @@
+var piezas = document.getElementsByClassName('movil');
+
+var tamWidh =[134,192,134,163,134,163,134,192,134];
+var tamHeight = [163,134,163,134,192,134,163,134,163];
+
+for(var i=0;i<piezas.length;i++){
+  piezas[i].setAttribute("width",tamWidh[i]);
+  piezas[i].setAttribute("height",tamHeight[i]);
+  piezas[i].setAttribute("x",Math.floor((Math.random() * 10)+ 1));
+  piezas[i].setAttribute("y",Math.floor((Math.random()* 409)+ 1));
+  piezas[i].setAttribute("onmousedown","seleccionarElemento(evt)");
+}
+
+var elementSelect = 0;
+var currentX = 0;
+var currentY = 0;
+var currentPosX = 0;
+var currentPosY = 0;
+
+function seleccionarElemento(evt){
+  elementSelect = evt.target;
+  currentX = evt.clientX;
+  currentY = evt.clientY;
+  currentPosx = parseFloat(elementSelect.getAttribute("x"));
+  currentPosy = parseFloat(elementSelect.getAttribute("y"));
+  elementSelect.setAttribute("onmousemove","moverElemento(evt)");
+
+}
+
+function moverElemento(evt){
+  var dx = evt.clientX - currentX;
+  var dy = evt.clientY - currentY;
+  currentPosx = currentPosx + dx;
+  currentPosy = currentPosy + dy;
+  elementSelect.setAttribute("x",currentPosX);
+  elementSelect.setAttribute("y",currentPosy);
+  currentX = evt.clientX;
+  currentY = evt.clientY;
+  elementSelect.setAttribute("onmouseout","deseleccionarElemento"(evt));
+  elementSelect.setAttribute("onmouseup","deselccionarElemento"(evt));
+}
+
+function desselccionarElemento(evt){
+  if(elementSelect != 0){
+    elementSelect.removeAttribute("onmousemuve");
+    elementSelect.removeAttribute("onmouseout");
+    elementSelect.removeAttribute("onmouseup");
+    elementSelect = 0;
+  }
+}
